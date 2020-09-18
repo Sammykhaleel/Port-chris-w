310f4febff4d3de42d4dafb3bc1f2ce2674eddcd
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2020-09-18 15:10:30 -0700

    pokemon loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 let pokemonList = [
-    { name: 'Charmander', height: 2, types: 'fire' },
-    { name: 'Blastoise', height: 5, types: 'water' },
-    { name: 'Slowbro', height: 5, types: ['water', 'psychic'] }
+    {
+        name: 'Charmander',
+        height: 2,
+        types: ['fire']
+    },
+    {
+        name: 'Blastoise',
+        height: 5.03,
+        types: ['water']
+    },
+    {
+        name: 'Slowbro',
+        height: 5.03,
+        types: ['water', 'psychic']
+    }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 2) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + " -Huge Pokemon!");
+    } else {
+        document.write(pokemonList.name + ' (height: ' + pokemonList[i].height + ')');
+    }
+}
